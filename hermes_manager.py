8b47b54b73018c74c9813c24b7dbea40fe72f685
Gideon Wamalwa <63095691+GidTheGreat@users.noreply.github.com>
2026-07-19 15:21:13 +0300

    added lifecycle management  to hermes manager

--- hermes_manager.py
+++ hermes_manager.py
@@ -13,16 +13,21 @@ class HermesManager:
     def add_thread(self, fn, *args, **kwargs):
         self.thread.start(fn, *args, **kwargs)
     
-    def add_task(self, fn, *args, **kwargs):
+    def add_task(self, name, fn, *args, **kwargs):
         if not self.event_loop.loop.is_running():
             print("not running,starting up")
-            self.start_event_loop()
-        self.event_loop.submit_task("", fn, *args, **kwargs)
+            #self.start_event_loop()
+        self.event_loop.submit_task(name, fn, *args, **kwargs)
         
     def add_periodic_task(self, interval, callback, *args, **kwargs):
         if not self.event_loop.loop.is_running():
-            self.start_event_loop()
+            print("loop not running")
+            #self.start_event_loop()
         PeriodicTask(self.event_loop, interval ,callback, *args, **kwargs).start()
+        
+    def stop(self):
+        self.event_loop.submit_task("stop", self.event_loop.stop)
+        self.thread.shutdown()
         
 
 async def test():
@@ -34,10 +39,26 @@ def test2():
     print("another joke")
     
 if __name__ == "__main__":
-    hm = HermesManager()
-    #hm.start_event_loop()
-    #hm.add_task(test)
-    hm.add_periodic_task(5, test2)
-    while True:
-        pass
+    import signal
+    import time
+    import log
+   
+    running = True
+    def handle(sig,frame):
+        global running
+        print("stop recieved")
+        running = False
+        
     
+    signal.signal(signal.SIGINT , handle)
+    hm = HermesManager()
+    hm.start_event_loop()
+    time.sleep(5)
+    #hm.add_task("test",test)
+    hm.add_periodic_task( 5, test2)
+    while running:
+        time.sleep(5)
+        
+    else:
+        hm.stop()
+   
